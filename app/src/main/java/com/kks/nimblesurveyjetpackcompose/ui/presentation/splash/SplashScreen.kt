6e82a9445b203b718d168224add40fee33bc6d6e
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-08 23:59:46 +0700

    parent 1665fd138fadc3c90c587b54e315692910aa3554 author Kaung Khant Soe <kaungkhantsoe.2712@gmail.com> 1659977986 +0700 committer Kaung Khant Soe <kaungkhantsoe.2712@gmail.com> 1660561131 +0700
    # This is a combination of 22 commits.
    # This is the 1st commit message:
    
    [#62] Integrate splash screen
    
    # This is the commit message #2:
    
    #[#5] Update build pull request workflow
    
    # This is the commit message #3:
    
    #[#5] Fix typo
    
    # This is the commit message #4:
    
    #[#4] Implement UI for Splash screen
    
    # This is the commit message #5:
    
    #[#4] Temporally suppress UnusedPrivateMember
    
    # This is the commit message #6:
    
    #[#4] Fix unresolved navgraphs
    
    # This is the commit message #7:
    
    #[#4] Convert splash_bg to webp
    
    # This is the commit message #8:
    
    #[#4] Remove unused args from splash screen
    
    # This is the commit message #9:
    
    #[#4] Extract string resource
    
    # This is the commit message #10:
    
    #[#63] Setup kover and remove jacoco
    
    # This is the commit message #11:
    
    #[#5] Setup firebase distribution for CD
    
    # This is the commit message #12:
    
    #[#4] Implement UI for Splash screen
    
    # This is the commit message #13:
    
    #[#63] Setup Kover and remove Jacoco
    
    # This is the commit message #14:
    
    #[#63] Temporally suppress UnusedPrivateMember
    
    # This is the commit message #15:
    
    #[#63] Remove redundant plugin
    
    # This is the commit message #16:
    
    #[#62] Integrate splash screen
    
    # This is the commit message #17:
    
    #[#62] Integrate splash screen
    
    # This is the commit message #18:
    
    #[#62] Integrate splash screen
    
    # This is the commit message #19:
    
    #[#62] Integrate splash screen
    
    # This is the commit message #20:
    
    #[#62] Rename test fun
    
    # This is the commit message #21:
    
    #[#62] Resolve conflict
    
    # This is the commit message #22:
    
    #[#5] Setup firebase distribution for CD
    
    #[#63] Setup Kover and remove Jacoco
    
    #[#63] Temporally suppress UnusedPrivateMember
    
    #[#63] Remove redundant plugin
    
    #[#62] Integrate splash screen
    
    #[#62] Integrate splash screen
    
    #[#62] Integrate splash screen
    
    #[#62] Integrate splash screen
    
    #[#62] Rename test fun
    
    #[#62] Update SplashScreen instrumented test for fail test
    
    #[#62] Remove google service

--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
@@ -12,13 +13,25 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kks.nimblesurveyjetpackcompose.R
-import com.kks.nimblesurveyjetpackcompose.ui.theme.NimbleSurveyJetpackComposeTheme
+import com.kks.nimblesurveyjetpackcompose.ui.presentation.destinations.LoginScreenDestination
+import com.kks.nimblesurveyjetpackcompose.viewmodel.splash.SplashViewModel
 import com.ramcosta.composedestinations.annotation.Destination
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
 @Destination(start = true)
 @Composable
-fun SplashScreen() {
+fun SplashScreen(navigator: DestinationsNavigator, viewModel: SplashViewModel = viewModel()) {
+    LaunchedEffect(
+        key1 = viewModel.shouldNavigateToLogin.value
+    ) {
+        if (viewModel.shouldNavigateToLogin.value) {
+            navigator.navigate(LoginScreenDestination)
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
@@ -32,15 +45,15 @@ fun SplashScreen() {
         Image(
             painter = painterResource(id = R.drawable.ic_logo_white),
             contentDescription = stringResource(id = R.string.splash_logo_content_description),
-            modifier = Modifier.size(201.0.dp, 48.0.dp)
+            modifier = Modifier
+                .size(201.0.dp, 48.0.dp)
         )
     }
+    viewModel.startTimerToNavigateToLogin()
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun DefaultPreview() {
-    NimbleSurveyJetpackComposeTheme {
-        SplashScreen()
-    }
+fun SplashPreview() {
+    SplashScreen(EmptyDestinationsNavigator)
 }
---
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/viewmodel/splash/SplashViewModel.kt
@@ -0,0 +1,22 @@
+package com.kks.nimblesurveyjetpackcompose.viewmodel.splash
+
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+const val SPLASH_TIME = 2000L
+class SplashViewModel(private val dispatcher: CoroutineDispatcher = Dispatchers.IO) : ViewModel() {
+
+    val shouldNavigateToLogin = mutableStateOf(false)
+
+    fun startTimerToNavigateToLogin() {
+        viewModelScope.launch(dispatcher) {
+            delay(SPLASH_TIME)
+            shouldNavigateToLogin.value = true
+        }
+    }
+}
