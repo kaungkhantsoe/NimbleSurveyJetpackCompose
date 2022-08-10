50dc567b1458cd2a889d0fcea0e32e295e5e7e66
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-10 12:52:55 +0700

    [#4] Implement UI for Splash screen

--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreen.kt
@@ -10,8 +10,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kks.nimblesurveyjetpackcompose.R
