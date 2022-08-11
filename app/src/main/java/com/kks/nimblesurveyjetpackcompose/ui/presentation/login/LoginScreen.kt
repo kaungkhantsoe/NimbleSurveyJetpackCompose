da1f7b729a5188deea7ba05ebe87edacb2000167
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-11 11:07:27 +0700

    [#62] Integrate splash screen

--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/login/LoginScreen.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/login/LoginScreen.kt
@@ -2,7 +2,12 @@ package com.kks.nimblesurveyjetpackcompose.ui.presentation.login
 
 import android.app.Activity
 import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import com.ramcosta.composedestinations.annotation.Destination
 
@@ -10,6 +15,14 @@ import com.ramcosta.composedestinations.annotation.Destination
 @Composable
 fun LoginScreen() {
     val activity = (LocalContext.current as? Activity)
+
+    // TODO: Remove this code when implementing UI for Login at https://github.com/kaungkhantsoe/NimbleSurveyJetpackCompose/issues/15
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(text = "Login")
+    }
 
     BackHandler {
         activity?.finish()
--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreen.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreen.kt
@@ -6,33 +6,57 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.kks.nimblesurveyjetpackcompose.R
+import com.kks.nimblesurveyjetpackcompose.ui.presentation.destinations.LoginScreenDestination
+import com.kks.nimblesurveyjetpackcompose.viewmodel.splash.SplashViewModel
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+
+const val SPLASH_BACKGROUND_IMAGE = "Splash Background"
+const val SPLASH_LOGO_IMAGE = "Nimble Logo"
 
 @Destination(start = true)
 @Composable
-fun SplashScreen(navigator: DestinationsNavigator) {
+fun SplashScreen(navigator: DestinationsNavigator, viewModel: SplashViewModel = viewModel()) {
+    LaunchedEffect(
+        key1 = viewModel.shouldNavigateToLogin.value
+    ) {
+        if (viewModel.shouldNavigateToLogin.value) {
+            navigator.navigate(LoginScreenDestination)
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
         Image(
             painter = painterResource(id = R.drawable.splash_bg),
-            contentDescription = "Splash Background",
+            contentDescription = SPLASH_BACKGROUND_IMAGE,
             modifier = Modifier.matchParentSize(),
             contentScale = ContentScale.Crop
         )
         Image(
             painter = painterResource(id = R.drawable.ic_logo_white),
-            contentDescription = "Nimble Logo",
+            contentDescription = SPLASH_LOGO_IMAGE,
             modifier = Modifier
                 .size(201.0.dp, 48.0.dp)
         )
     }
+    viewModel.startTimerToNavigateToLogin()
+}
+
+@Preview
+@Composable
+fun SplashPreview() {
+    SplashScreen(EmptyDestinationsNavigator)
 }
