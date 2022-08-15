c4e3ef1d0735ea9a9c43bab29617bfd8a36db4f3
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-15 17:49:14 +0700

    [#62] Update SplashScreen instrumented test for fail test

--- app/src/androidTest/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreenTest.kt
+++ app/src/androidTest/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/splash/SplashScreenTest.kt
@@ -1,9 +1,12 @@
 package com.kks.nimblesurveyjetpackcompose.ui.presentation.splash
 
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.kks.nimblesurveyjetpackcompose.R
+import com.kks.nimblesurveyjetpackcompose.ui.presentation.main.MainActivity
 import com.kks.nimblesurveyjetpackcompose.ui.theme.NimbleSurveyJetpackComposeTheme
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 import org.junit.Rule
@@ -14,7 +17,7 @@ import org.junit.runner.RunWith
 class SplashScreenTest {
 
     @get:Rule
-    val composeTestRule = createComposeRule()
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
 
     @Test
     fun when_splash_screen_start_show_both_background_image_and_logo_image() {
@@ -23,7 +26,9 @@ class SplashScreenTest {
                 SplashScreen(EmptyDestinationsNavigator)
             }
         }
-        composeTestRule.onNodeWithContentDescription(SPLASH_BACKGROUND_IMAGE).assertIsDisplayed()
-        composeTestRule.onNodeWithContentDescription(SPLASH_LOGO_IMAGE).assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.splash_background_content_description))
+            .assertIsDisplayed()
+        composeTestRule.onNodeWithContentDescription(composeTestRule.activity.getString(R.string.splash_logo_content_description))
+            .assertIsDisplayed()
     }
 }
