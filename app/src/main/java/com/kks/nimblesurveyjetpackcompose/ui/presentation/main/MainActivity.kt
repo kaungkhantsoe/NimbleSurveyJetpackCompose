b3fbd8bae8b9f186bd2e52aa198111b1ca2b0e5b
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-15 13:13:53 +0700

    [#4] Fix unresolved navgraphs

--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/main/MainActivity.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/ui/presentation/main/MainActivity.kt
@@ -6,7 +6,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
-import com.kks.nimblesurveyjetpackcompose.ui.presentation.NavGraphs
+import com.kks.nimblesurveyjetpackcompose.ui.presentation.splash.NavGraphs
 import com.kks.nimblesurveyjetpackcompose.ui.theme.NimbleSurveyJetpackComposeTheme
 import com.ramcosta.composedestinations.DestinationsNavHost
 
