60b3fbb5083b2de60e461dc6429ef6300ff499bd
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-22 21:20:45 +0700

    [#8] Update UI test and unit test

--- app/src/test/java/com/kks/nimblesurveyjetpackcompose/viewmodel/splash/SplashViewModelTest.kt
+++ app/src/test/java/com/kks/nimblesurveyjetpackcompose/viewmodel/splash/SplashViewModelTest.kt
@@ -4,13 +4,13 @@ import com.kks.nimblesurveyjetpackcompose.base.BaseViewModelTest
 import com.kks.nimblesurveyjetpackcompose.model.ResourceState
 import com.kks.nimblesurveyjetpackcompose.model.response.LoginResponse
 import com.kks.nimblesurveyjetpackcompose.repo.login.LoginRepo
-import io.mockk.coEvery
 import io.mockk.mockk
 import org.junit.Assert.assertEquals
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.advanceUntilIdle
 import kotlinx.coroutines.test.runTest
 import org.junit.Test
@@ -46,7 +46,7 @@ class SplashViewModelTest : BaseViewModelTest() {
 
             assertEquals(true, actual)
         }
-    }
+}
 
     @Test
     fun `When login with incorrect email or password, show error`() = runTest {
