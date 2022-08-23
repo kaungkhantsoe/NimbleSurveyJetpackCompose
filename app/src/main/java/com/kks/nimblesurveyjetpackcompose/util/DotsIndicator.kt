d1d2cb4ec29b1ef8b614a2e20b5b2206ee328d79
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-24 02:45:11 +0700

    [#9] Implement home screen UI

---
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/util/DotsIndicator.kt
@@ -0,0 +1,54 @@
+package com.kks.nimblesurveyjetpackcompose.util
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+// Reference https://medium.com/@prashantappdeveloper/viewpager-in-jetpack-compose-with-dot-indicators-within-minutes-a2779970534e
+@Suppress("LongParameterList")
+@Composable
+fun DotsIndicator(
+    totalDots: Int,
+    selectedIndex: Int,
+    selectedColor: Color,
+    unSelectedColor: Color,
+    indicatorSize: Dp,
+    space: Dp
+) {
+    LazyRow(
+        modifier = Modifier
+            .wrapContentWidth()
+            .wrapContentHeight()
+            .padding(start = 20.dp, end = 20.dp)
+
+    ) {
+        items(totalDots) { index ->
+            if (index == selectedIndex) {
+                Box(
+                    modifier = Modifier
+                        .size(indicatorSize)
+                        .clip(CircleShape)
+                        .background(selectedColor)
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .size(indicatorSize)
+                        .clip(CircleShape)
+                        .background(unSelectedColor)
+                )
+            }
+
+            if (index != totalDots - 1) {
+                Spacer(modifier = Modifier.padding(horizontal = space))
+            }
+        }
+    }
+}
