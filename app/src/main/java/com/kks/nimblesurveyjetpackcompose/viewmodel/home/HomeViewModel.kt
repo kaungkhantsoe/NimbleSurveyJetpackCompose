577d82f30d0fb00268e68165ba87ea143be902e2
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-24 15:13:41 +0700

    [#9] Add pull refresh

--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/viewmodel/home/HomeViewModel.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/viewmodel/home/HomeViewModel.kt
@@ -1,27 +1,38 @@
 package com.kks.nimblesurveyjetpackcompose.viewmodel.home
 
 import androidx.lifecycle.ViewModel
-import com.kks.nimblesurveyjetpackcompose.model.ErrorModel
-import com.kks.nimblesurveyjetpackcompose.model.response.SurveyResponse
+import androidx.lifecycle.viewModelScope
+import com.kks.nimblesurveyjetpackcompose.di.IoDispatcher
+import com.kks.nimblesurveyjetpackcompose.repo.home.HomeRepo
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class HomeViewModel @Inject constructor() : ViewModel() {
+class HomeViewModel @Inject constructor(
+    private val homeRepo: HomeRepo,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
+) : ViewModel() {
 
     private val _isRefreshing = MutableStateFlow(false)
-    private var _error = MutableStateFlow<ErrorModel?>(null)
-    private var _surveyList = MutableStateFlow<ArrayList<SurveyResponse>>(arrayListOf())
-
-    val error: StateFlow<ErrorModel?>
-        get() = _error.asStateFlow()
-
-    val surveyList: StateFlow<ArrayList<SurveyResponse>>
-        get() = _surveyList.asStateFlow()
 
     val isRefreshing: StateFlow<Boolean>
         get() = _isRefreshing.asStateFlow()
+
+    fun getUserDetail() {
+        viewModelScope.launch(ioDispatcher) {
+            homeRepo.fetchUserDetail().collect { }
+        }
+    }
+
+    fun getSurveyList(pageNumber: Int, pageSize: Int) {
+        viewModelScope.launch(ioDispatcher) {
+            homeRepo.fetchSurveyList(pageNumber, pageSize).collect { }
+        }
+    }
 }
