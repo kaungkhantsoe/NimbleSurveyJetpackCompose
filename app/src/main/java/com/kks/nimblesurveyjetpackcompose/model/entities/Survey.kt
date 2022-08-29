661145b410644808c0ce2552f5203ca5d72aa399
Kaung Khant Soe <kaungkhantsoe.2712@gmail.com>
2022-08-29 13:10:18 +0700

    [#18] Add column info to Survey

--- app/src/main/java/com/kks/nimblesurveyjetpackcompose/model/entities/Survey.kt
+++ app/src/main/java/com/kks/nimblesurveyjetpackcompose/model/entities/Survey.kt
@@ -1,13 +1,18 @@
 package com.kks.nimblesurveyjetpackcompose.model.entities
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "survey")
 data class Survey(
     @PrimaryKey
+    @ColumnInfo(name = "id")
     val id: String,
+    @ColumnInfo(name = "coverImageUrl")
     val coverImageUrl: String,
+    @ColumnInfo(name = "title")
     val title: String,
+    @ColumnInfo(name = "description")
     val description: String
 )
